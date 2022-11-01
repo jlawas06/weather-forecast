2f98e78b7b34d7ca7a384588db3285c017fd7091
John Lloyd Lawas <johnlloydlawas06@gmail.com>
2022-11-01 16:31:04 +0800

    Implement auth guard for home page

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
+
 import { HomeComponent } from './pages/home/home.component';
 import { LandingComponent } from './pages/landing/landing.component';
 
@@ -10,7 +12,8 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
